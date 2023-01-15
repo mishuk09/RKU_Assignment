fab922b91652de7cec18b25506da3ecb216f7494
mishuk09 <mishukinfo09@gmail.com>
2023-01-15 04:55:52 -0500

    Assignment-01

--- Java/Exprement_1/exprement_01.java
+++ Java/Exprement_1/exprement_01.java
@@ -21,9 +21,6 @@ public class exprement_01 {
 
 */
 
-
-
-
 // 2.  Write a Program to print the area of triangle. 
 
 /*package Java.Exprement_1;
@@ -46,3 +43,87 @@ public class exprement_01 {
 
 
 */
+
+// 3.Program to Check Whether an Alphabet is Vowel or Consonant
+
+
+
+
+
+/*  
+package Java.Exprement_1;
+
+import java.util.*;
+
+public class exprement_01 {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        char ch= sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Your char is Voil");
+
+        } else {
+            System.out.println("Ypur char is consonent");
+        }
+
+    }
+
+}
+*/
+
+
+
+// 4...Program to Find ASCII Value of a character
+
+
+
+
+/* 
+
+package Java.Exprement_1;
+
+import java.util.*;
+
+public class exprement_01 {
+
+   public static void main(String args[]){
+     char ch='A';
+     int ASCII=ch;
+     System.out.println(ASCII);
+
+   }
+    
+
+}
+
+
+*/
+
+
+// 5. Java Program to Display Armstrong Number Between Two Intervals
+
+
+
+package Java.Exprement_1;
+
+import java.util.*;
+
+public class exprement_01 {
+
+   public static void main(String args[]){
+    
+    int num=123;
+
+    
+
+
+
+
+
+   }
+    
+
+}
