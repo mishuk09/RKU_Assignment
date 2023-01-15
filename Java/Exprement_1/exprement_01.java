4bfb3b78f8ff73e19067cc8075ed3747e2346b41
mishuk09 <mishukinfo09@gmail.com>
2023-01-15 03:15:25 -0500

    Exprement-01

---
+++ Java/Exprement_1/exprement_01.java
@@ -0,0 +1,48 @@
+
+// 1. Program to Print an Integer (Entered by the User)
+
+/*package Java.Exprement_1;
+
+import java.util.Scanner;
+
+public class exprement_01 {
+
+    public static void main(String args[]) {
+        System.out.print("Enter number:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(num);
+
+    }
+
+}
+
+
+*/
+
+
+
+
+// 2.  Write a Program to print the area of triangle. 
+
+/*package Java.Exprement_1;
+ 
+
+public class exprement_01 {
+
+    public static void main(String args[]) {
+      
+        double  height=4;
+        double width=5;
+
+        double are = 0.5 * height * width;
+
+        System.out.println(are);
+
+    }
+
+}
+
+
+*/
