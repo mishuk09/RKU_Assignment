072ee7073af7224b6caf503c43d68a7880aa84d7
mishuk09 <mishukinfo09@gmail.com>
2023-01-21 13:29:55 -0500

    python lab practice

--- Python/Regular Expression/Exprement-01.py
+++ Python/Regular Expression/Exprement-01.py
@@ -1,24 +1,29 @@
 
 
-# Regular expression
+#Regular expression
 
 
-# There are many regular expression
-# findall,  search,  split,  sub,  finditer
+#There are many regular expression
+#findall,  search,  split,  sub,  finditer
 
 
 # match
 
+
+
 # import re
 # name = r"color"
-
 # if re.match(name, "Color red is my fav color"):
 #     print("Match")
 # else:
 #     print("Not match")
 
 
+
+
 # search
+
+
 
 # import re
 # name = r"Hasan"
@@ -29,9 +34,16 @@
 #     print("Not match")
 
 
+
+
 # findall
+
+
 
 import re
 name = r"Mahadi"
 
 print(re.findall(name, "My name is Mahadi Hasan Mishuk.You can call me Mahadi"))
+
+
+
