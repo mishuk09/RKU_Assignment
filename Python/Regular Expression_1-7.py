4609037fb6eea8bf594890028beaa2b4cd39f803
mishuk09 <mishukinfo09@gmail.com>
2023-01-21 13:12:48 -0500

    python lab practice

--- Python/Regular Expression_1-7.py
+++ Python/Regular Expression_1-7.py
@@ -9,10 +9,29 @@
 
 # match
 
-import re
-name = r"color"
+# import re
+# name = r"color"
 
-if re.match(name, "Color red is my fav color"):
-    print("Match")
-else:
-    print("Not match")
+# if re.match(name, "Color red is my fav color"):
+#     print("Match")
+# else:
+#     print("Not match")
+
+
+# search
+
+# import re
+# name = r"Hasan"
+
+# if re.search(name, ("My name is Mahadi Hasan Mishuk.You can call Me Mahadi")):
+#     print("Match")
+# else:
+#     print("Not match")
+
+
+# findall
+
+import re
+name = r"Mahadi"
+
+print(re.findall(name, "My name is Mahadi Hasan Mishuk.You can call me Mahadi"))
