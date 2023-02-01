4adc7a91afa26f4f73d0608caf53ba8083d09be4
mishuk09 <mishukinfo09@gmail.com>
2023-02-01 12:05:41 -0500

    portfolio

--- Java/Exprement_1/exprement_01.java
+++ Java/Exprement_1/exprement_01.java
@@ -149,165 +149,143 @@ public class exprement_01 {
 
 // Java Program to Make a Simple Calculator Using switch...case
 
-import java.util.*;
+// import java.util.*;
 
-public class exprement_01 {
+// public class exprement_01 {
 
-    public static void main(String args[]) {
+//     public static void main(String args[]) {
 
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        System.out.println("Enter operator:");
-        char ch = sc.next().charAt(0);
+//         Scanner sc = new Scanner(System.in);
+//         int num1 = sc.nextInt();
+//         int num2 = sc.nextInt();
+//         System.out.println("Enter operator:");
+//         char ch = sc.next().charAt(0);
 
-        switch (ch) {
-            case '+':
-                System.out.println("Sum of two number is" + " " + (num1 + num2));
-            case '-':
-                System.out.println("Sum of two number is" + " " + (num1 - num2));
-            case '*':
-                System.out.println("Sum of two number is" + " " + (num1 * num2));
-            case '/':
-                System.out.println("Sum of two number is" + " " + (num1 / num2));
+//         switch (ch) {
+//             case '+':
+//                 System.out.println("Sum of two number is" + " " + (num1 + num2));
+//             case '-':
+//                 System.out.println("Sum of two number is" + " " + (num1 - num2));
+//             case '*':
+//                 System.out.println("Sum of two number is" + " " + (num1 * num2));
+//             case '/':
+//                 System.out.println("Sum of two number is" + " " + (num1 / num2));
 
-        }
+//         }
 
-    }
+//     }
 
-}
+// }
 
-// Write a java Program to check the number is Prime or not.
+// Write a java Program to check the number is Prime or not
 
-/*
- * 
- * package Java.Exprement_1;
- * 
- * import java.util.*;
- * 
- * public class exprement_01 {
- * 
- * public static void main(String args[]) {
- * 
- * Scanner sc = new Scanner(System.in);
- * System.out.print("Enter number:");
- * int num = sc.nextInt();
- * boolean isPrime = true;
- * 
- * for (int i = 2; i < num - 1; i++) {
- * if (num % i == 0) {
- * isPrime = false;
- * }
- * 
- * }
- * 
- * if (isPrime == true) {
- * System.out.println("Prime number");
- * } else {
- * System.out.println("Not prime number");
- * }
- * 
- * }
- * 
- * }
- * 
- */
+// import java.util.*;
 
-// Java Program to Calculate Average Using Arrays
+// public class exprement_01 {
 
-/*
- * package Java.Exprement_1;
- * 
- * import java.util.*;
- * 
- * public class exprement_01 {
- * 
- * public static void main(String args[]) {
- * 
- * int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
- * int sum = 0;
- * 
- * for (int i = 0; i < arr.length+1; i++) {
- * sum += i;
- * 
- * }
- * System.out.println(sum);
- * 
- * }
- * 
- * }
- * 
- */
+//     public static void main(String args[]) {
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number:");
+//         int num = sc.nextInt();
+//         boolean isPrime = true;
+
+//         for (int i = 2; i < num - 1; i++) {
+//             if (num % i == 0) {
+//                 isPrime = false;
+//             }
+
+//         }
+
+//         if (isPrime == true) {
+//             System.out.println("Prime number");
+//         } else {
+//             System.out.println("Not prime number");
+//         }
+
+//     }
+
+// }
+
+//Java Program to Calculate Average Using Array
+
+// import java.util.*;
+
+// public class exprement_01 {
+
+//     public static void main(String args[]) {
+
+//         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//         int sum = 0;
+
+//         for (int i = 0; i < arr.length + 1; i++) {
+//             sum += i;
+
+//         }
+//         System.out.println(sum);
+
+//     }
+
+// }
 
 // Java Program to Find Largest Element of an Array
 
-/*
- * package Java.Exprement_1;
- * 
- * import java.util.*;
- * 
- * public class exprement_01 {
- * 
- * public static void main(String args[]) {
- * 
- * int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
- * int maxEle = arr[0];
- * 
- * for (int i = 0; i < arr.length; i++) {
- * if (arr[i] > maxEle) {
- * maxEle = arr[i];
- * }
- * 
- * }
- * System.out.println(maxEle);
- * 
- * }
- * 
- * }
- * 
- */
+// import java.util.*;
+
+// public class exprement_01 {
+
+//     public static void main(String args[]) {
+
+//         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//         int maxEle = arr[0];
+
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] > maxEle) {
+//                 maxEle = arr[i];
+//             }
+
+//         }
+//         System.out.println(maxEle);
+
+//     }
+
+// }
 
 // 10. Java Program to Add Two Matrix Using Multi-dimensional Arrays
 
-/*
- * 
- * package Java.Exprement_1;
- * 
- * import java.util.*;
- * 
- * public class exprement_01 {
- * 
- * public static void main(String args[]) {
- * Scanner sc = new Scanner(System.in);
- * 
- * System.out.print("Enter Row and Col: ");
- * int row = sc.nextInt();
- * int col = sc.nextInt();
- * 
- * int arr[][] = new int[row][col];
- * 
- * System.out.println("Enter Element:");
- * 
- * for (int i = 0; i < row; i++) {
- * for (int j = 0; j < col; j++) {
- * arr[i][j] = sc.nextInt();
- * }
- * 
- * }
- * 
- * System.out.println("Output");
- * for (int i = 0; i < row; i++) {
- * for (int j = 0; j < col; j++) {
- * 
- * System.out.print(arr[i][j] + " ");
- * }
- * System.out.println();
- * }
- * 
- * }
- * }
- * 
- */
+// import java.util.*;
+
+// public class exprement_01 {
+
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter Row and Col: ");
+//         int row = sc.nextInt();
+//         int col = sc.nextInt();
+
+//         int arr[][] = new int[row][col];
+
+//         System.out.println("Enter Element:");
+
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+
+//         }
+
+//         System.out.println("Output");
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
 
 // 17...Java Program to convert char type variables to integer.
 
