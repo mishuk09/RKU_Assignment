4ba7c40f8f8e224d781a115954cb4455aa4e14f1
mishuk09 <mishukinfo09@gmail.com>
2023-04-09 23:24:13 -0400

    python tkinter

---
+++ Shel_Script/index.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Hello world"
+
+echo $BASH 
+echo $BASH_VERSION
+echo $HOME
+echo $PWS
+
+
+name=Mahadi
+
+echo This is my name my name is the best name $name
---
+++ Shel_Script/variable.sh
@@ -0,0 +1,10 @@
+num1=10
+num2=20
+
+echo $num1
+echo $num2
+echo $num1+$num2
+
+echo "Enter your name below:"
+read name
+echo "Name : $name"
