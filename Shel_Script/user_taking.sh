fb69640d9dd721c9f8f1812415787a130e089cf1
mishuk09 <mishukinfo09@gmail.com>
2023-04-21 20:19:30 -0400

    SH

---
+++ Shel_Script/user_taking.sh
@@ -0,0 +1,12 @@
+# echo "Enter your name:"
+ 
+
+# read  name name2 name3
+
+# echo "$name,$name2,$name3"
+
+read -p 'username:' user_var
+read -sp 'Password:' pass_var
+
+echo "$user_var"
+echo "$pass_var"
